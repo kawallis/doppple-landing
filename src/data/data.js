53050bd929d9cc49d85a311c5d17402d00c445ff
Christopher Wallace <christopherwallace@Christophers-MacBook-Pro.local>
2018-08-12 17:09:39 -0700

    added new message

--- src/data/data.js
+++ src/data/data.js
@@ -12,7 +12,11 @@ export const accordian = [
     desc: `Shipping a beta version to a limited alpha group.`
   },
   {
+    title: `Are guides paid?`,
+    desc: `Yes guides are paid so you should signup to become one!`
+  },
+  {
     title: `How can I become a guide?`,
-    desc: `It's invite-only for now. So if you drop your email in the box above, we'll put you on the list for a future invite.`
+    desc: `To become a guide fill in the subscribe form above that way we can send you a link for a short form to fill out!`
   }
 ]
